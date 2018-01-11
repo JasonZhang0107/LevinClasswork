2b44cf1ea35048cdb0a5c6da34bd23571c28fdb0
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2018-01-11 13:13:21 -0500

    Added timer

--- Mr. Levin Classwork/src/Lab4Three/GameCode.java
+++ Mr. Levin Classwork/src/Lab4Three/GameCode.java
@@ -1,5 +1,9 @@
 package Lab4Three;
 
+import java.awt.Label;
+import java.awt.TextField;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -19,17 +23,37 @@ public class GameCode extends Application
     @Override
     public void start(Stage primaryStage) 
     {
+    	//Set title of stage
         primaryStage.setTitle("Fast Clicker");
+        
+        //Button events and text
         Button btn = new Button();
         btn.setText("Click me");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-        	
+            
             @Override
             public void handle(ActionEvent event) 
             {
                 GameCode.score++;
+                
+                long timeStep = System.nanoTime() + 10000000000L;
+                new AnimationTimer()
+                {
+                	@Override
+                	public void handle(long now) {
+                		if (now > timeStep)
+                		{
+                			System.out.println("You clicked " + score + " times in 10 seconds");
+                			System.exit(0);
+                		}
+                	}
+                }.start();
             }
         });
+        
+        TextField name = new TextField();
+        
+        
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 250));
